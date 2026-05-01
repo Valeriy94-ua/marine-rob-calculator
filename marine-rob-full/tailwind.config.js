6171e514215eb67b68e822be4309eb6ae2159c01
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2026-05-01 10:50:12 +0300

    Delete marine-rob-full directory

--- marine-rob-full/tailwind.config.js
+++
@@ -1,1 +0,0 @@
-export default {\n  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],\n  theme: {\n    extend: {},\n  },\n  plugins: [],\n}\n
--- marine-rob-full/vite.config.ts
+++
@@ -1,1 +0,0 @@
-import { defineConfig } from 'vite'\nimport react from '@vitejs/plugin-react'\n\nexport default defineConfig({\n  plugins: [react()],\n  server: {\n    port: 5173,\n  },\n})\n
